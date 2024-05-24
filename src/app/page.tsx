431744a47a9541c089ffa9b7541a8d851f3d41e3
ali <alimanssouri221@gmail.com>
2024-05-24 21:18:01 +0330

    chore: Remove unused code and update Home component
    The Home component in page.tsx has been updated to remove unused code, including the next/image import and the calculateLength and greeting functions. This improves code cleanliness and reduces unnecessary complexity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
-import Image from "next/image";
 
-/**
- * Renders the Home component.
- * 
- * @returns The rendered Home component.
- */
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       Docs
     </div>
   );
-}
-
-// function that calculates the length of a string
-function calculateLength(string: string) {
-  return string.length;
-}
-
-// function that returns a greeting message
-function greeting(name: string) {
-  return `Hello, ${name}!`;
 }
