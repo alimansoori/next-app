596be0afce0cd3cb87dda111608e7e9c11dfcf51
ali <alimanssouri221@gmail.com>
2024-05-27 01:17:06 +0330

    chore: Add Redux store and provider
    This commit adds the necessary code changes to implement Redux in the application. It includes the addition of the Redux store and provider components, as well as the counterSlice and its associated actions. This will allow for state management and data flow in the application.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+'use client';
+
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./globalRedux/store";
+import { decrement, increment, incrementByAmount } from "./globalRedux/features/counter/counterSlice";
 
 export default function Home() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      Docs
-    </div>
+    <main>
+      <div>
+        <button
+          onClick={() => dispatch(increment())}
+        >Increment</button>
+      </div>
+      <div>
+        <span>{count}</span>
+      </div>
+      <div>
+        <button
+          onClick={() => dispatch(decrement())}
+        >Decrement</button>
+      </div>
+      <div>
+        <button
+          onClick={() => dispatch(incrementByAmount(2))}
+        >Increment by 2</button>
+      </div>
+    </main>
   );
 }
